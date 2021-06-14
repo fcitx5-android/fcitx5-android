b142c35e7b8047c3b69285d6e2c663e99e3807d9
Potato Hatsue <1793913507@qq.com>
2021-06-14 13:31:22 +0000

    Use environment variable ECM_DIR if possible

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    ext.kotlin_version = "1.5.10"
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:4.2.1'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:4.2.1")
+        classpath(kotlin("gradle-plugin", version =e "1.5.10"))
 
         // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
+        // in the individual module build.gradle.kts files
     }
 }
 
@@ -21,6 +20,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
-include ':app'
+include (":app")
 rootProject.name = "Fcitx5Test"
