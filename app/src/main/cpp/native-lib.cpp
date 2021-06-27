c4aed9e54ce594255e0ff7d11d0105ec9cc72c24
Vifly <viflythink@gmail.com>
2021-06-27 22:47:55 +0800

    move method implement

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -16,52 +16,46 @@ static const size_t androidBufSize = 512;
 
 class AndroidStreamBuf : public std::streambuf {
 public:
-    explicit AndroidStreamBuf(size_t buf_size);
-    ~AndroidStreamBuf() override;
+    AndroidStreamBuf(size_t buf_size) : buf_size_(buf_size) {
+        assert(buf_size_ > 0);
+        pbuf_ = new char[buf_size_];
+        memset(pbuf_, 0, buf_size_);
 
-    int overflow(int c) override;
-    int sync() override;
+        setp(pbuf_, pbuf_ + buf_size_);
+    }
+
+    ~AndroidStreamBuf() { delete pbuf_; }
+
+    int overflow(int c) {
+        if (-1 == sync()) {
+            return traits_type::eof();
+        } else {
+            // put c into buffer after successful sync
+            if (!traits_type::eq_int_type(c, traits_type::eof())) {
+                sputc(traits_type::to_char_type(c));
+            }
+
+            return traits_type::not_eof(c);
+        }
+    }
+
+    int sync() {
+        auto str_buf = fcitx::stringutils::trim(std::string(pbuf_));
+        auto trim_pbuf = str_buf.c_str();
+
+        int res = __android_log_write(ANDROID_LOG_DEBUG, tag, trim_pbuf);
+
+        memset(pbuf_, 0, buf_size_);
+        setp(pbase(), pbase() + buf_size_);
+        pbump(0);
+        return res;
+    }
 
 private:
     const size_t buf_size_;
     char *pbuf_;
 };
 
-
-AndroidStreamBuf::AndroidStreamBuf(size_t buf_size) : buf_size_(buf_size) {
-    assert(buf_size_ > 0);
-    pbuf_ = new char[buf_size_];
-    memset(pbuf_, 0, buf_size_);
-
-    setp(pbuf_, pbuf_ + buf_size_);
-}
-
-AndroidStreamBuf::~AndroidStreamBuf() { delete pbuf_; }
-
-int AndroidStreamBuf::sync() {
-    auto str_buf = fcitx::stringutils::trim(std::string(pbuf_));
-    auto trim_pbuf = str_buf.c_str();
-
-    int res = __android_log_write(ANDROID_LOG_DEBUG, tag, trim_pbuf);
-
-    memset(pbuf_, 0, buf_size_);
-    setp(pbase(), pbase() + buf_size_);
-    pbump(0);
-    return res;
-}
-
-int AndroidStreamBuf::overflow(int c) {
-    if (-1 == sync()) {
-        return traits_type::eof();
-    } else {
-        // put c into buffer after successful sync
-        if (!traits_type::eq_int_type(c, traits_type::eof())) {
-            sputc(traits_type::to_char_type(c));
-        }
-
-        return traits_type::not_eof(c);
-    }
-}
 
 static void jniLog(const std::string &s) {
     __android_log_write(ANDROID_LOG_DEBUG, "JNI", s.c_str());
