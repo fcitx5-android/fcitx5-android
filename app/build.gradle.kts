bfab7978defad2b1377687e9870b7f2677259d1a
Rocka <i@rocka.me>
2021-07-14 23:15:24 +0800

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,15 +53,15 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.5.0")
+    implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.appcompat:appcompat:1.3.0")
-    implementation("com.google.android.material:material:1.3.0")
+    implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
     implementation("androidx.lifecycle", "lifecycle-runtime-ktx", "2.3.1")
     implementation("androidx.lifecycle:lifecycle-service:2.3.1")
     implementation("androidx.lifecycle:lifecycle-common-java8:2.3.1")
     implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-android", "1.4.2")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 }
