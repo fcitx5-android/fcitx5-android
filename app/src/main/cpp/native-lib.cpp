318917047b79662657c9c316ffcf8924e6776ea6
Rocka <i@rocka.me>
2021-08-07 18:59:41 +0800

    extract `AndroidStreamBuf`  to a separate file

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -11,52 +11,7 @@
 #include <fcitx-utils/stringutils.h>
 
 #include "androidfrontend/androidfrontend_public.h"
-
-static const char *tag = "fcitx5";
-static const size_t androidBufSize = 512;
-
-class AndroidStreamBuf : public std::streambuf {
-public:
-    explicit AndroidStreamBuf(size_t buf_size) : buf_size_(buf_size) {
-        assert(buf_size_ > 0);
-        pbuf_ = new char[buf_size_];
-        memset(pbuf_, 0, buf_size_);
-
-        setp(pbuf_, pbuf_ + buf_size_);
-    }
-
-    ~AndroidStreamBuf() override { delete pbuf_; }
-
-    int overflow(int c) override {
-        if (-1 == sync()) {
-            return traits_type::eof();
-        } else {
-            // put c into buffer after successful sync
-            if (!traits_type::eq_int_type(c, traits_type::eof())) {
-                sputc(traits_type::to_char_type(c));
-            }
-
-            return traits_type::not_eof(c);
-        }
-    }
-
-    int sync() override {
-        auto str_buf = fcitx::stringutils::trim(std::string(pbuf_));
-        auto trim_pbuf = str_buf.c_str();
-
-        int res = __android_log_write(ANDROID_LOG_DEBUG, tag, trim_pbuf);
-
-        memset(pbuf_, 0, buf_size_);
-        setp(pbase(), pbase() + buf_size_);
-        pbump(0);
-        return res;
-    }
-
-private:
-    const size_t buf_size_;
-    char *pbuf_;
-};
-
+#include "androidstreambuf.h"
 
 class Fcitx {
 public:
@@ -369,7 +324,7 @@ std::string jstringToString(JNIEnv *env, jstring j) {
 
 JNIEXPORT jint JNICALL
 JNI_OnLoad(JavaVM * /* jvm */, void * /* reserved */) {
-    static std::ostream stream(new AndroidStreamBuf(androidBufSize));
+    static std::ostream stream(new AndroidStreamBuf("fcitx5", 512));
     fcitx::Log::setLogStream(stream);
     // return supported JNI version; or it will crash
     return JNI_VERSION_1_6;
