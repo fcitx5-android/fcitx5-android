728e11098a4918af9f5b2fa8263d54192052bad4
Rocka <i@rocka.me>
2021-11-19 18:57:28 +0800

    update build tools and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,9 +4,9 @@ plugins {
 }
 
 android {
-    compileSdk = 30
-    buildToolsVersion = "30.0.3"
-    ndkVersion = "23.0.7599858"
+    compileSdk = 31
+    buildToolsVersion = "31.0.0"
+    ndkVersion = "23.1.7779620"
 
     defaultConfig {
         applicationId = "me.rocka.fcitx5test"
@@ -53,17 +53,17 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.2")
     implementation("androidx.lifecycle", "lifecycle-runtime-ktx", "2.3.1")
-    implementation("androidx.lifecycle:lifecycle-service:2.3.1")
-    implementation("androidx.lifecycle:lifecycle-common-java8:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-service:2.4.0")
+    implementation("androidx.lifecycle:lifecycle-common-java8:2.4.0")
     implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-android", "1.4.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test:runner:1.4.0")
     androidTestImplementation("androidx.test:rules:1.4.0")
-    androidTestImplementation("androidx.lifecycle:lifecycle-runtime-testing:2.3.1")
+    androidTestImplementation("androidx.lifecycle:lifecycle-runtime-testing:2.4.0")
     androidTestImplementation("junit:junit:4.13.2")
 }
