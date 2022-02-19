972d880a61fd08d4e19bc4cc3ba5c2897508a632
Potato Hatsue <1793913507@qq.com>
2022-02-19 15:33:11 -0500

    Auto detect span count

--- app/src/main/java/me/rocka/fcitx5test/keyboard/CandidateViewAdapter.kt
+++ app/src/main/java/me/rocka/fcitx5test/keyboard/CandidateViewAdapter.kt
@@ -3,13 +3,17 @@ package me.rocka.fcitx5test.keyboard
 import android.annotation.SuppressLint
 import android.graphics.Paint
 import android.graphics.Rect
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import me.rocka.fcitx5test.R
 import splitties.dimensions.dp
+import splitties.resources.dimen
+import splitties.resources.dimenPxSize
 import splitties.views.dsl.core.matchParent
 import splitties.views.dsl.core.textView
 import splitties.views.gravityCenter
@@ -28,6 +32,8 @@ abstract class CandidateViewAdapter :
             field = value
             notifyDataSetChanged()
         }
+
+    // cache measureWidth
     private val measuredWidths = mutableMapOf<String, Float>()
 
     fun measureWidth(position: Int): Float {
@@ -35,6 +41,7 @@ abstract class CandidateViewAdapter :
         return measuredWidths[candidate] ?: run {
             val paint = Paint()
             val bounds = Rect()
+            // 20f here is chosen randomly, since we only care about the ratio
             paint.textSize = 20f
             paint.getTextBounds(candidate, 0, candidate.length, bounds)
             (bounds.width() / 20f).also { measuredWidths[candidate] = it }
@@ -45,9 +52,9 @@ abstract class CandidateViewAdapter :
         val view = parent.context.textView {
             layoutParams = GridLayoutManager.LayoutParams(matchParent, dp(40))
             gravity = gravityCenter
-            horizontalPadding = dp(8)
-            textSize = 20f // sp
-            minWidth = dp(50)
+            horizontalPadding = dimenPxSize(R.dimen.candidate_padding)
+            setTextSize(TypedValue.COMPLEX_UNIT_PX, dimen(R.dimen.candidate_font_size))
+            minWidth = dimenPxSize(R.dimen.candidate_min_width)
             isSingleLine = true
         }
         return ViewHolder(view).apply {
