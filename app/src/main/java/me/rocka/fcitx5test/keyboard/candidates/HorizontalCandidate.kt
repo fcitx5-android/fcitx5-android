57296452cd02b065a805ef5cf2f200dd31992c29
Potato Hatsue <1793913507@qq.com>
2022-02-25 17:14:41 -0500

    Update the data of expanded candidate when horizontal candidate layout finished

--- app/src/main/java/me/rocka/fcitx5test/keyboard/candidates/HorizontalCandidate.kt
+++ app/src/main/java/me/rocka/fcitx5test/keyboard/candidates/HorizontalCandidate.kt
@@ -1,8 +1,10 @@
 package me.rocka.fcitx5test.keyboard.candidates
 
 import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
 import me.rocka.fcitx5test.R
 import me.rocka.fcitx5test.utils.dependency.context
+import me.rocka.fcitx5test.utils.globalLayoutListener
 import org.mechdancer.dependency.Dependent
 import org.mechdancer.dependency.UniqueComponent
 import org.mechdancer.dependency.manager.ManagedHandler
@@ -11,14 +13,26 @@ import org.mechdancer.dependency.manager.must
 import splitties.resources.styledColor
 import splitties.views.backgroundColor
 import splitties.views.dsl.recyclerview.recyclerView
+import java.util.concurrent.atomic.AtomicBoolean
 
 class HorizontalCandidate : UniqueComponent<HorizontalCandidate>(), Dependent,
     ManagedHandler by managedHandler() {
 
     private val builder: CandidateViewBuilder by manager.must()
     private val context: Context by manager.context()
+    private val expandedCandidate: ExpandedCandidate by manager.must()
 
-    val adapter by lazy { builder.newCandidateViewAdapter() }
+    private var needsRefreshExpanded = AtomicBoolean(false)
+    val adapter by lazy {
+        builder.newCandidateViewAdapter().also {
+            it.registerAdapterDataObserver(object :
+                RecyclerView.AdapterDataObserver() {
+                override fun onChanged() {
+                    needsRefreshExpanded.set(true)
+                }
+            })
+        }
+    }
     val recyclerView by lazy {
         context.recyclerView(R.id.candidate_view) {
             isVerticalScrollBarEnabled = false
@@ -27,6 +41,14 @@ class HorizontalCandidate : UniqueComponent<HorizontalCandidate>(), Dependent,
                 autoSpanCount()
                 setupGridLayoutManager(this@HorizontalCandidate.adapter, false)
                 addGridDecoration()
+            }
+            globalLayoutListener {
+                if (needsRefreshExpanded.compareAndSet(true, false)) {
+                    val candidates = this@HorizontalCandidate.adapter.candidates
+                    expandedCandidate.adapter.updateCandidates(
+                        candidates.sliceArray(childCount until candidates.size)
+                    )
+                }
             }
         }
     }
