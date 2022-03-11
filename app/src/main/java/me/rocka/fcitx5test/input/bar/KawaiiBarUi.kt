b0d45dccd81d6cc51c8ccc7564526c772c912e29
Rocka <i@rocka.me>
2022-03-11 19:00:00 +0800

    layout IdleBar tool buttons

--- app/src/main/java/me/rocka/fcitx5test/input/bar/KawaiiBarUi.kt
+++ app/src/main/java/me/rocka/fcitx5test/input/bar/KawaiiBarUi.kt
@@ -1,12 +1,17 @@
 package me.rocka.fcitx5test.input.bar
 
 import android.content.Context
+import android.graphics.Typeface
 import android.view.View
+import android.widget.ImageView
+import androidx.annotation.DrawableRes
 import androidx.constraintlayout.widget.ConstraintLayout
 import me.rocka.fcitx5test.R
 import splitties.dimensions.dp
+import splitties.resources.styledDrawable
 import splitties.views.dsl.constraintlayout.*
 import splitties.views.dsl.core.*
+import splitties.views.gravityVerticalCenter
 import splitties.views.imageResource
 
 sealed class KawaiiBarUi(override val ctx: Context) : Ui {
@@ -37,28 +42,45 @@ sealed class KawaiiBarUi(override val ctx: Context) : Ui {
 
     class Idle(ctx: Context) : KawaiiBarUi(ctx) {
 
-        val expandMenuButton = imageButton {
-            background = null
-            imageResource = R.drawable.ic_baseline_expand_more_24
-            rotation = -90f
+        private fun toolButton(@DrawableRes icon: Int) = imageButton {
+            imageResource = icon
+            background = styledDrawable(android.R.attr.actionBarItemBackground)
+            scaleType = ImageView.ScaleType.CENTER_INSIDE
         }
 
+        val undoButton = toolButton(R.drawable.ic_baseline_undo_24)
+
+        val redoButton = toolButton(R.drawable.ic_baseline_redo_24)
+
+        val pasteButton = toolButton(R.drawable.ic_baseline_content_paste_24)
+
+        val clipboardButton = toolButton(R.drawable.ic_outline_assignment_24)
+
+        val settingsButton = toolButton(R.drawable.ic_baseline_settings_24)
+
         val hideKeyboardButton = imageButton {
-            background = null
+            background = styledDrawable(android.R.attr.actionBarItemBackground)
             imageResource = R.drawable.ic_baseline_arrow_drop_down_24
         }
 
+        private fun ConstraintLayout.addButton(
+            v: View,
+            initParams: ConstraintLayout.LayoutParams.() -> Unit = {}
+        ) {
+            add(v, ConstraintLayout.LayoutParams(dp(40), dp(40)).apply {
+                topOfParent()
+                bottomOfParent()
+                initParams(this)
+            })
+        }
+
         override val root = ctx.constraintLayout {
-            add(expandMenuButton, lParams(dp(40), dp(40)) {
-                topOfParent()
-                startOfParent()
-                bottomOfParent()
-            })
-            add(hideKeyboardButton, lParams(dp(40), dp(40)) {
-                topOfParent()
-                endOfParent()
-                bottomOfParent()
-            })
+            addButton(undoButton) { startOfParent(); before(redoButton) }
+            addButton(redoButton) { after(undoButton); before(pasteButton) }
+            addButton(pasteButton) { after(redoButton); before(clipboardButton) }
+            addButton(clipboardButton) { after(pasteButton); before(settingsButton) }
+            addButton(settingsButton) { after(clipboardButton); before(hideKeyboardButton) }
+            addButton(hideKeyboardButton) { endOfParent() }
         }
     }
 
@@ -66,10 +88,13 @@ sealed class KawaiiBarUi(override val ctx: Context) : Ui {
 
         private val backButton = imageButton {
             imageResource = R.drawable.ic_baseline_arrow_back_24
-            background = null
+            background = styledDrawable(android.R.attr.actionBarItemBackground)
+            scaleType = ImageView.ScaleType.CENTER_INSIDE
         }
 
         private val titleText = textView {
+            setTypeface(null, Typeface.BOLD)
+            gravity = gravityVerticalCenter
             textSize = 16f
         }
 
@@ -83,7 +108,7 @@ sealed class KawaiiBarUi(override val ctx: Context) : Ui {
             })
             add(titleText, lParams(wrapContent, dp(40)) {
                 topOfParent()
-                after(backButton, dp(20))
+                after(backButton, dp(8))
                 bottomOfParent()
             })
         }
