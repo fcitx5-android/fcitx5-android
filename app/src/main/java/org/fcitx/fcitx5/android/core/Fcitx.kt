0797cee51c9c2f6652a537853996e9a049cc5900
Rocka <i@rocka.me>
2022-04-09 00:21:01 +0800

    Activate StatusArea Action in JVM

--- app/src/main/java/org/fcitx/fcitx5/android/core/Fcitx.kt
+++ app/src/main/java/org/fcitx/fcitx5/android/core/Fcitx.kt
@@ -110,6 +110,9 @@ class Fcitx(private val context: Context) : FcitxLifecycleOwner by JNI {
     suspend fun setCapFlags(flags: CapabilityFlags) =
         withFcitxContext { setCapabilityFlags(flags.toLong()) }
 
+    suspend fun activateAction(id: Int) =
+        withFcitxContext { activateUserInterfaceAction(id) }
+
     init {
         if (lifecycle.currentState != FcitxLifecycle.State.STOPPED)
             throw IllegalAccessException("Fcitx5 is already created!")
@@ -243,6 +246,9 @@ class Fcitx(private val context: Context) : FcitxLifecycleOwner by JNI {
 
         @JvmStatic
         external fun setCapabilityFlags(flags: Long)
+
+        @JvmStatic
+        external fun activateUserInterfaceAction(id: Int)
 
         @JvmStatic
         external fun loopOnce()
