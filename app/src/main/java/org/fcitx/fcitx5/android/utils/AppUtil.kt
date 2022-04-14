5fcdbf7226185659b685797735c02e8e195700d7
Rocka <i@rocka.me>
2022-04-14 19:52:10 +0800

    Support open sepcific fragment when launch MainActivity

--- app/src/main/java/org/fcitx/fcitx5/android/utils/AppUtil.kt
+++ app/src/main/java/org/fcitx/fcitx5/android/utils/AppUtil.kt
@@ -2,33 +2,40 @@ package org.fcitx.fcitx5.android.utils
 
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
 import org.fcitx.fcitx5.android.ui.main.LogActivity
 import org.fcitx.fcitx5.android.ui.main.MainActivity
 
 object AppUtil {
-    fun launchMain(context: Context) {
+    fun launchMain(context: Context, initIntent: Intent.() -> Unit = {}) {
         context.startActivity(
-            Intent(
-                context,
-                MainActivity::class.java
-            ).apply { addFlags(Intent.FLAG_ACTIVITY_NEW_TASK) })
+            Intent(context, MainActivity::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                initIntent.invoke(this)
+            }
+        )
     }
 
-    fun launchLog(context: Context) {
+    fun launchLog(context: Context, initIntent: Intent.() -> Unit = {}) {
         context.startActivity(
-            Intent(
-                context,
-                LogActivity::class.java
-            ).apply {
+            Intent(context, LogActivity::class.java).apply {
                 putExtra(LogActivity.NOT_CRASH, 0)
-            })
+                initIntent.invoke(this)
+            }
+        )
     }
 
     fun launchMainToAddInputMethods(context: Context) {
-        context.startActivity(Intent(context, MainActivity::class.java).apply {
-            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        launchMain(context) {
             putExtra(MainActivity.INTENT_DATA_ADD_IM, 0)
-        })
+        }
+    }
+
+    fun launchMainToConfig(context: Context, category: String, arguments: Bundle? = null) {
+        launchMain(context) {
+            putExtra(MainActivity.INTENT_DATA_CONFIG, category)
+            putExtra(category, arguments)
+        }
     }
 
 }
