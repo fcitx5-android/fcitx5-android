39d5653558f06b55aeb239920ea9dc5dac3d2322
Rocka <i@rocka.me>
2022-04-25 21:41:14 +0800

    Theme management UI

---
+++ app/src/main/java/org/fcitx/fcitx5/android/ui/main/settings/theme/ChooseImageEntryUi.kt
@@ -0,0 +1,40 @@
+package org.fcitx.fcitx5.android.ui.main.settings.theme
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
+import androidx.constraintlayout.widget.ConstraintLayout
+import org.fcitx.fcitx5.android.R
+import splitties.dimensions.dp
+import splitties.resources.drawable
+import splitties.views.dsl.constraintlayout.*
+import splitties.views.dsl.core.*
+import splitties.views.imageDrawable
+
+class ChooseImageEntryUi(override val ctx: Context) : Ui {
+    val text = textView {
+        setText(R.string.choose_image)
+        setTextColor(Color.WHITE)
+    }
+
+    val icon = imageView {
+        imageDrawable = ctx.drawable(R.drawable.ic_baseline_photo_library_24)
+        colorFilter = PorterDuffColorFilter(Color.WHITE, PorterDuff.Mode.SRC_IN)
+    }
+
+    override val root = constraintLayout {
+        background = ctx.drawable(R.drawable.bkg_theme_choose_image)
+        add(icon, lParams(dp(24), dp(24)) {
+            topOfParent()
+            centerHorizontally()
+            above(text, dp(8))
+            verticalChainStyle = ConstraintLayout.LayoutParams.CHAIN_PACKED
+        })
+        add(text, lParams(wrapContent, wrapContent) {
+            below(icon)
+            centerHorizontally()
+            bottomOfParent()
+        })
+    }
+}
