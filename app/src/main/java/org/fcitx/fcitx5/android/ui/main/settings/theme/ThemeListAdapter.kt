335d091500d664a19d5eadc7bcc19a3ec81739c5
Rocka <i@rocka.me>
2022-04-26 21:31:36 +0800

    Highlight activate theme; Edit custom theme

--- app/src/main/java/org/fcitx/fcitx5/android/ui/main/settings/theme/ThemeListAdapter.kt
+++ app/src/main/java/org/fcitx/fcitx5/android/ui/main/settings/theme/ThemeListAdapter.kt
@@ -1,10 +1,9 @@
 package org.fcitx.fcitx5.android.ui.main.settings.theme
 
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import org.fcitx.fcitx5.android.data.theme.Theme
-import splitties.dimensions.dp
+import org.fcitx.fcitx5.android.data.theme.ThemeManager
 import splitties.views.dsl.core.Ui
 
 abstract class ThemeListAdapter : RecyclerView.Adapter<ThemeListAdapter.ViewHolder>() {
@@ -12,7 +11,24 @@ abstract class ThemeListAdapter : RecyclerView.Adapter<ThemeListAdapter.ViewHold
 
     val entries = mutableListOf<Theme>()
 
-    private fun entryAt(position: Int) = entries[position - 1]
+    private fun entryAt(position: Int) = entries[position - OFFSET]
+
+    fun setThemes(themes: List<Theme>) {
+        entries.clear()
+        entries.addAll(themes)
+        notifyItemRangeInserted(OFFSET, themes.size)
+    }
+
+    fun prependTheme(it: Theme) {
+        entries.add(0, it)
+        notifyItemInserted(OFFSET)
+    }
+
+    fun replaceTheme(theme: Theme) {
+        val index = entries.indexOfFirst { it.name.contentEquals(theme.name) }
+        entries[index] = theme
+        notifyItemChanged(index + OFFSET)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         ViewHolder(
@@ -20,8 +36,6 @@ abstract class ThemeListAdapter : RecyclerView.Adapter<ThemeListAdapter.ViewHold
                 CHOOSE_IMAGE -> ChooseImageEntryUi(parent.context)
                 THEME -> ThemeThumbnailUi(parent.context)
                 else -> throw IllegalArgumentException(INVALID_TYPE + viewType)
-            }.apply {
-                root.layoutParams = parent.context.run { ViewGroup.LayoutParams(dp(128), dp(88)) }
             }
         )
 
@@ -30,8 +44,11 @@ abstract class ThemeListAdapter : RecyclerView.Adapter<ThemeListAdapter.ViewHold
             CHOOSE_IMAGE -> holder.ui.root.setOnClickListener { onChooseImage() }
             THEME -> (holder.ui as ThemeThumbnailUi).apply {
                 entryAt(position).let { theme ->
-                    setTheme(theme)
+                    setTheme(theme, theme.name.contentEquals(ThemeManager.currentTheme.name))
                     root.setOnClickListener { onSelectTheme(theme) }
+                    editButton.setOnClickListener {
+                        if (theme is Theme.Custom) onEditTheme(theme)
+                    }
                 }
             }
             else -> throw IllegalArgumentException(INVALID_TYPE + it)
@@ -46,7 +63,11 @@ abstract class ThemeListAdapter : RecyclerView.Adapter<ThemeListAdapter.ViewHold
 
     abstract fun onSelectTheme(theme: Theme)
 
+    abstract fun onEditTheme(theme: Theme.Custom)
+
     companion object {
+        const val OFFSET = 1
+
         const val CHOOSE_IMAGE = 0
         const val THEME = 1
 
