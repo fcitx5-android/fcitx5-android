f2f162a139271c377063f1a53c9f942e34db32d9
keta1 <k@ketal.icu>
2022-06-01 17:49:34 +0800

    Migrate kapt to ksp

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,20 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://plugins.gradle.org/m2/")
+        maven("https://jitpack.io")
+    }
+}
+
 include (":app")
 rootProject.name = "fcitx5-android"
