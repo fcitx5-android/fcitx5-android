0a471b04290948e0e72b00487d089aac16b13560
Rocka <i@rocka.me>
2022-07-05 18:34:56 +0800

    Add simple linear PopupKeyboardUi

--- app/src/main/java/org/fcitx/fcitx5/android/input/popup/PopupComponent.kt
+++ app/src/main/java/org/fcitx/fcitx5/android/input/popup/PopupComponent.kt
@@ -9,6 +9,8 @@ import org.fcitx.fcitx5.android.data.theme.ThemeManager
 import org.fcitx.fcitx5.android.input.dependency.context
 import org.fcitx.fcitx5.android.input.dependency.inputMethodService
 import org.fcitx.fcitx5.android.input.dependency.theme
+import org.fcitx.fcitx5.android.input.keyboard.KeyAction
+import org.fcitx.fcitx5.android.input.keyboard.KeyDef
 import org.mechdancer.dependency.Dependent
 import org.mechdancer.dependency.UniqueComponent
 import org.mechdancer.dependency.manager.ManagedHandler
@@ -30,6 +32,8 @@ class PopupComponent :
     private val dismissJobs = HashMap<Int, Job>()
     private val freeEntryUi = LinkedList<PopupEntryUi>()
 
+    private val showingKeyboardUi = HashMap<Int, PopupKeyboardUi>()
+
     private val keyBottomMargin by lazy {
         context.dp(ThemeManager.prefs.keyVerticalMargin.getValue())
     }
@@ -44,7 +48,7 @@ class PopupComponent :
     }
     private val hideThreshold = 100L
 
-    val view by lazy {
+    val root by lazy {
         context.frameLayout {
             isClickable = false
             isFocusable = false
@@ -65,13 +69,43 @@ class PopupComponent :
             lastShowTime = System.currentTimeMillis()
             setText(content)
         }
-        view.apply {
+        root.apply {
             add(popup.root, lParams(popupWidth, popupHeight) {
                 topMargin = bounds.bottom - popupHeight - keyBottomMargin
                 leftMargin = (bounds.left + bounds.right - popupWidth) / 2
             })
         }
         showingEntryUi[viewId] = popup
+    }
+
+    fun showKeyboard(viewId: Int, keyboard: KeyDef.Popup.Keyboard, bounds: Rect) {
+        val popupEntryUi = showingEntryUi[viewId] ?: run {
+            showPopup(viewId, "", bounds)
+            showingEntryUi.getValue(viewId)
+        }
+        popupEntryUi.apply {
+            val (x, y) = intArrayOf(0, 0).also { root.getLocationInWindow(it) }
+            val keyboardUi = PopupKeyboardUi(ctx, theme, popupRadius, keyboard)
+            showingKeyboardUi[viewId] = keyboardUi
+            this@PopupComponent.root.apply {
+                add(keyboardUi.root, lParams {
+                    leftMargin = x
+                    topMargin = y
+                })
+            }
+        }
+    }
+
+    fun changeFocus(viewId: Int, deltaX: Int, deltaY: Int): Boolean {
+        showingKeyboardUi[viewId]?.apply {
+            moveFocus(deltaX, deltaY)
+            return true
+        }
+        return false
+    }
+
+    fun triggerFocusedKeyboard(viewId: Int): KeyAction? {
+        return showingKeyboardUi[viewId]?.trigger()
     }
 
     fun dismissPopup(viewId: Int) {
@@ -87,11 +121,15 @@ class PopupComponent :
                 }
             }
         }
+        showingKeyboardUi[viewId]?.also {
+            showingKeyboardUi.remove(viewId)
+            root.removeView(it.root)
+        }
     }
 
     private fun reallyDismissPopup(viewId: Int, popup: PopupEntryUi) {
         showingEntryUi.remove(viewId)
-        view.removeView(popup.root)
+        root.removeView(popup.root)
         freeEntryUi.add(popup)
     }
 
