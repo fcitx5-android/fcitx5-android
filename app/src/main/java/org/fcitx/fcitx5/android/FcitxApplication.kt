034d2086d9186af95e2d17c1cd6a9899a07b5c38
Rocka <i@rocka.me>
2022-09-24 17:48:48 +0800

    Don't exit application when locale change

--- app/src/main/java/org/fcitx/fcitx5/android/FcitxApplication.kt
+++ app/src/main/java/org/fcitx/fcitx5/android/FcitxApplication.kt
@@ -1,10 +1,6 @@
 package org.fcitx.fcitx5.android
 
 import android.app.Application
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.content.res.Configuration
 import android.os.Process
 import android.util.Log
@@ -16,7 +12,6 @@ import org.fcitx.fcitx5.android.data.theme.ThemeManager
 import org.fcitx.fcitx5.android.ui.main.LogActivity
 import org.fcitx.fcitx5.android.utils.isDarkMode
 import timber.log.Timber
-import kotlin.system.exitProcess
 
 class FcitxApplication : Application() {
     override fun onCreate() {
@@ -45,13 +40,6 @@ class FcitxApplication : Application() {
         }
 
         AppPrefs.init(sharedPrefs, resources)
-
-        registerReceiver(object : BroadcastReceiver() {
-            override fun onReceive(context: Context, intent: Intent) {
-                Timber.i("Detected a locale change, process will exit now")
-                exitProcess(0)
-            }
-        }, IntentFilter(Intent.ACTION_LOCALE_CHANGED))
         // record last pid for crash logs
         AppPrefs.getInstance().internal.pid.apply {
             val currentPid = Process.myPid()
