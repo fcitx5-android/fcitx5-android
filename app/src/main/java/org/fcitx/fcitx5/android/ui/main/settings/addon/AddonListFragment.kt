be70cd56156d9997f113fa007bf4506be996916f
Potato Hatsue <1793913507@qq.com>
2022-11-09 13:01:49 -0500

    Consider dependents on disabling addons

--- app/src/main/java/org/fcitx/fcitx5/android/ui/main/settings/addon/AddonListFragment.kt
+++ app/src/main/java/org/fcitx/fcitx5/android/ui/main/settings/addon/AddonListFragment.kt
@@ -1,11 +1,14 @@
 package org.fcitx.fcitx5.android.ui.main.settings.addon
 
 import android.view.View
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AlertDialog
 import androidx.core.os.bundleOf
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import org.fcitx.fcitx5.android.R
 import org.fcitx.fcitx5.android.core.AddonInfo
+import org.fcitx.fcitx5.android.core.FcitxAPI
 import org.fcitx.fcitx5.android.daemon.launchOnFcitxReady
 import org.fcitx.fcitx5.android.ui.common.BaseDynamicListUi
 import org.fcitx.fcitx5.android.ui.common.CheckBoxListUi
@@ -19,6 +22,8 @@ class AddonListFragment : ProgressFragment(), OnItemChangedListener<AddonInfo> {
 
     private lateinit var ui: BaseDynamicListUi<AddonInfo>
 
+    private val addonDisplayNames = mutableMapOf<String, String>()
+
     private fun updateAddonState() {
         if (!isInitialized)
             return
@@ -31,9 +36,73 @@ class AddonListFragment : ProgressFragment(), OnItemChangedListener<AddonInfo> {
         }
     }
 
+    private fun disableAddon(entry: AddonInfo, reset: () -> Unit) {
+        val dependents = fcitx
+            .runImmediately { getAddonReverseDependencies(entry.uniqueName) }
+        if (dependents.isNotEmpty()) {
+            fun f(depTy: FcitxAPI.AddonDep) =
+                dependents.mapNotNull {
+                    it.takeIf { x -> x.second == depTy }
+                        ?.first
+                        ?.let { u -> it.first to (addonDisplayNames[u] ?: u) }
+                }
+
+            fun mkStr(
+                list: List<Pair<String, String>>,
+                @StringRes template: Int
+            ) =
+                list.takeIf { it.isNotEmpty() }
+                    ?.joinToString(", ") { it.second }
+                    ?.let { getString(template, it) }
+
+            val dep = f(FcitxAPI.AddonDep.Required)
+            val depU = dep.map { it.first }.toSet()
+            val depStr = mkStr(dep, R.string.disable_addon_warn_dep)
+            val optDepStr = mkStr(f(FcitxAPI.AddonDep.Optional), R.string.disable_addon_warn_optdep)
+
+            if (depStr != null || optDepStr != null) {
+                val msg = buildString {
+                    depStr?.let {
+                        append("- ")
+                        appendLine(it)
+                    }
+                    optDepStr?.let {
+                        appendLine()
+                        append("- ")
+                        appendLine(it)
+                    }
+                }
+                AlertDialog.Builder(requireContext())
+                    .setTitle(getString(R.string.disable_addon_warn_title, entry.displayName))
+                    .setIcon(R.drawable.ic_baseline_error_24)
+                    .setMessage(msg)
+                    .setCancelable(false)
+                    .setNegativeButton(android.R.string.cancel) { _, _ ->
+                        reset()
+                    }
+                    .setPositiveButton(android.R.string.ok) { _, _ ->
+                        entries.mapIndexedNotNull { idx, x ->
+                            x.takeIf { it.uniqueName in depU }?.uniqueName?.let { idx }
+                        }.forEach {
+                            // TODO: combine update addon states
+                            ui.updateItem(it, entries[it].copy(enabled = false))
+                        }
+                        ui.updateItem(ui.indexItem(entry), entry.copy(enabled = false))
+                    }
+                    .show()
+            } else {
+                ui.updateItem(ui.indexItem(entry), entry.copy(enabled = false))
+            }
+        }
+    }
+
     override suspend fun initialize(): View {
         ui = requireContext().CheckBoxListUi(
-            initialEntries = fcitx.runOnReady { addons().sortedBy { it.uniqueName } },
+            initialEntries = fcitx.runOnReady {
+                addons()
+                    .sortedBy { it.uniqueName }
+                    .onEach { addonDisplayNames[it.uniqueName] = it.displayName }
+            },
             initCheckBox = { entry ->
                 // remove old listener before change checked state
                 setOnCheckedChangeListener(null)
@@ -41,7 +110,10 @@ class AddonListFragment : ProgressFragment(), OnItemChangedListener<AddonInfo> {
                 isEnabled = entry.uniqueName != "androidfrontend"
                 isChecked = entry.enabled
                 setOnCheckedChangeListener { _, isChecked ->
-                    ui.updateItem(ui.indexItem(entry), entry.copy(enabled = isChecked))
+                    if (!isChecked)
+                        disableAddon(entry) { this.isChecked = true }
+                    else
+                        ui.updateItem(ui.indexItem(entry), entry.copy(enabled = true))
                 }
             },
             initSettingsButton = { entry ->
