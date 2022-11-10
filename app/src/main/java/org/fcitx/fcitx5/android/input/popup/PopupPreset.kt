0a4f7d59e8132760701806bb0c4287bb89c2ea06
Rocka <i@rocka.me>
2022-11-10 21:38:24 +0800

    Tweak period long press symbol list

--- app/src/main/java/org/fcitx/fcitx5/android/input/popup/PopupPreset.kt
+++ app/src/main/java/org/fcitx/fcitx5/android/input/popup/PopupPreset.kt
@@ -129,7 +129,7 @@ val PopupPreset: Map<String, Array<String>> = hashMapOf(
     //
     // Punctuation
     //
-    "." to arrayOf("…", "?", "!", ":", ";", "_", "%", "$", "^", "&"),
+    "." to arrayOf(",", "?", "!", ":", ";", "_", "%", "$", "^", "&"),
     "-" to arrayOf("—", "–", "·"),
     "?" to arrayOf("¿", "‽"),
     "'" to arrayOf("‘", "’", "‚", "›", "‹"),
