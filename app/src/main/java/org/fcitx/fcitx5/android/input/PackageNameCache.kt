07a4fb09e2134e1c33fd3a6d8170d9fb89b19b0a
Rocka <i@rocka.me>
2023-02-02 14:03:49 +0800

    Strip ":uid" in "package name" for Apps using sharedUserId

--- app/src/main/java/org/fcitx/fcitx5/android/input/PackageNameCache.kt
+++ app/src/main/java/org/fcitx/fcitx5/android/input/PackageNameCache.kt
@@ -6,10 +6,13 @@ class PackageNameCache(private val ctx: Context) : HashMap<Int, String>() {
     fun forUid(uid: Int): String {
         val cached = get(uid)
         if (cached != null) return cached
+        // returns "${sharedUserIdName}:${uid}" rather than package name if app uses sharedUserId
         val name = ctx.packageManager.getNameForUid(uid)
         if (name != null) {
-            put(uid, name)
-            return name
+            // strip :uid to make it constant across devices
+            val pkgName = name.substringBeforeLast(':')
+            put(uid, pkgName)
+            return pkgName
         }
         return uid.toString()
     }
