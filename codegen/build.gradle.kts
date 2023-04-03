a16e5ebdccb23743a227ae65927c9b858f781a22
Potato Hatsue <1793913507@qq.com>
2023-04-03 18:13:20 -0400

    Use jvmToolchain in :codegen

--- codegen/build.gradle.kts
+++ codegen/build.gradle.kts
@@ -2,12 +2,11 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-
 dependencies {
     implementation(libs.kotlinpoet.ksp)
     implementation(libs.ksp)
+}
+
+kotlin {
+    jvmToolchain(11)
 }
