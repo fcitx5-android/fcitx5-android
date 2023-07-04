9edc1659c9581ed83a09ec6ba84932ff39d7e91c
Rocka <i@rocka.me>
2023-07-04 13:43:22 +0800

    Mark broadcast constant `@JvmField`

--- lib/common/src/main/java/org/fcitx/fcitx5/android/common/Broadcasts.kt
+++ lib/common/src/main/java/org/fcitx/fcitx5/android/common/Broadcasts.kt
@@ -2,8 +2,15 @@ package org.fcitx.fcitx5.android.common
 
 @Suppress("PropertyName")
 class Broadcasts(applicationId: String) {
+    @JvmField
     val FcitxInputMethodServiceCreated = "$applicationId.IME_SERVICE_CREATED"
+
+    @JvmField
     val FcitxInputMethodServiceDestroyed = "$applicationId.IME_SERVICE_DESTROYED"
+
+    @JvmField
     val FcitxApplicationCreated = "$applicationId.APP_CREATED"
+
+    @JvmField
     val PERMISSION = "$applicationId.permission.BROADCAST"
 }
