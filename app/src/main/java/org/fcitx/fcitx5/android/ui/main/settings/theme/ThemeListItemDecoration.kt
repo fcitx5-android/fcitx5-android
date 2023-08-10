4755c75538080b8d299c3e32dae354e13c9a1648
Rocka <i@rocka.me>
2023-08-10 23:47:02 +0800

    Fix theme list item offset of last row

--- app/src/main/java/org/fcitx/fcitx5/android/ui/main/settings/theme/ThemeListItemDecoration.kt
+++ app/src/main/java/org/fcitx/fcitx5/android/ui/main/settings/theme/ThemeListItemDecoration.kt
@@ -3,6 +3,7 @@ package org.fcitx.fcitx5.android.ui.main.settings.theme
 import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.ceil
 
 class ThemeListItemDecoration(val itemWidth: Int, val spanCount: Int) :
     RecyclerView.ItemDecoration() {
@@ -16,7 +17,7 @@ class ThemeListItemDecoration(val itemWidth: Int, val spanCount: Int) :
         val offset = (parent.width - itemWidth * spanCount) / (spanCount + 1)
         val halfOffset = offset / 2
         val position = parent.getChildAdapterPosition(view)
-        val rowCount = parent.adapter?.run { itemCount / spanCount } ?: -1
+        val rowCount = parent.adapter?.run { ceil(itemCount / spanCount.toFloat()).toInt() } ?: -1
         val n = position % spanCount
 
         when (parent.layoutDirection) {
