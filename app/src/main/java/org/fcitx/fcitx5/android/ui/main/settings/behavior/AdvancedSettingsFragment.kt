04353453d37708a18a832babb4a59ee3b9bb479a
Rocka <i@rocka.me>
2023-08-21 21:16:11 +0800

    Stop fcitx instance before import; exit App after import

--- app/src/main/java/org/fcitx/fcitx5/android/ui/main/settings/behavior/AdvancedSettingsFragment.kt
+++ app/src/main/java/org/fcitx/fcitx5/android/ui/main/settings/behavior/AdvancedSettingsFragment.kt
@@ -1,26 +1,36 @@
 package org.fcitx.fcitx5.android.ui.main.settings.behavior
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.NotificationCompat
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.preference.PreferenceScreen
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.NonCancellable
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.fcitx.fcitx5.android.R
+import org.fcitx.fcitx5.android.daemon.FcitxDaemon
 import org.fcitx.fcitx5.android.data.UserDataManager
 import org.fcitx.fcitx5.android.data.prefs.AppPrefs
 import org.fcitx.fcitx5.android.data.prefs.ManagedPreferenceFragment
 import org.fcitx.fcitx5.android.ui.common.withLoadingDialog
+import org.fcitx.fcitx5.android.ui.main.MainActivity
 import org.fcitx.fcitx5.android.ui.main.MainViewModel
 import org.fcitx.fcitx5.android.utils.AppUtil
 import org.fcitx.fcitx5.android.utils.addPreference
 import org.fcitx.fcitx5.android.utils.errorDialog
 import org.fcitx.fcitx5.android.utils.iso8601UTCDateTime
+import org.fcitx.fcitx5.android.utils.notificationManager
 import org.fcitx.fcitx5.android.utils.queryFileName
 import org.fcitx.fcitx5.android.utils.toast
 
@@ -51,25 +61,41 @@ class AdvancedSettingsFragment : ManagedPreferenceFragment(AppPrefs.getInstance(
                             return@withContext
                         }
                         try {
+                            // stop fcitx before overwriting files
+                            FcitxDaemon.stopFcitx()
                             val inputStream = cr.openInputStream(uri)!!
                             val metadata = UserDataManager.import(inputStream).getOrThrow()
+                            lifecycleScope.launch(NonCancellable + Dispatchers.Main) {
+                                delay(400L)
+                                AppUtil.exit()
+                            }
                             withContext(Dispatchers.Main) {
-                                AlertDialog.Builder(ctx)
-                                    .setTitle(R.string.import_user_data)
-                                    .setMessage(
-                                        getString(
-                                            R.string.user_data_imported,
-                                            iso8601UTCDateTime(metadata.exportTime)
+                                NotificationCompat.Builder(ctx, CHANNEL_ID)
+                                    .setSmallIcon(R.drawable.ic_baseline_sync_24)
+                                    .setContentTitle(getText(R.string.app_name))
+                                    .setContentText(getText(R.string.restart_notify_msg))
+                                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                                    .setContentIntent(
+                                        PendingIntent.getActivity(
+                                            ctx,
+                                            0,
+                                            Intent(ctx, MainActivity::class.java),
+                                            PendingIntent.FLAG_IMMUTABLE
                                         )
                                     )
-                                    .setPositiveButton(android.R.string.ok) { _, _ ->
-                                        AppUtil.exit()
-                                    }
-                                    .setNegativeButton(android.R.string.cancel, null)
-                                    .setIconAttribute(android.R.attr.alertDialogIcon)
-                                    .show()
+                                    .setAutoCancel(true)
+                                    .build()
+                                    .let { ctx.notificationManager.notify(NOTIFY_ID, it) }
+                                ctx.toast(
+                                    getString(
+                                        R.string.user_data_imported,
+                                        iso8601UTCDateTime(metadata.exportTime)
+                                    )
+                                )
                             }
                         } catch (e: Exception) {
+                            // re-start fcitx in case importing failed
+                            FcitxDaemon.startFcitx()
                             importErrorDialog(e.localizedMessage ?: e.stackTraceToString())
                         }
                     }
@@ -93,6 +119,7 @@ class AdvancedSettingsFragment : ManagedPreferenceFragment(AppPrefs.getInstance(
                     }
                 }
             }
+        createNotificationChannel()
     }
 
     override fun onPreferenceUiCreated(screen: PreferenceScreen) {
@@ -109,11 +136,35 @@ class AdvancedSettingsFragment : ManagedPreferenceFragment(AppPrefs.getInstance(
             }
         }
         screen.addPreference(R.string.import_user_data) {
-            importLauncher.launch("application/zip")
+            val ctx = requireContext()
+            AlertDialog.Builder(ctx)
+                .setIconAttribute(android.R.attr.alertDialogIcon)
+                .setTitle(R.string.import_user_data)
+                .setMessage(R.string.confirm_import_user_data)
+                .setPositiveButton(android.R.string.ok) { _, _ ->
+                    importLauncher.launch("application/zip")
+                }
+                .setNegativeButton(android.R.string.cancel, null)
+                .show()
         }
     }
 
     private suspend fun importErrorDialog(message: String) {
         errorDialog(requireContext(), getString(R.string.import_error), message)
+    }
+
+    private val CHANNEL_ID = "app-restart"
+
+    private val NOTIFY_ID = 0xdead
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                getText(R.string.restart_channel),
+                NotificationManager.IMPORTANCE_HIGH
+            ).apply { description = CHANNEL_ID }
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
