980cda7e58fc8f3ca18a7f0582623ff2a936bd20
Potato Hatsue <1793913507@qq.com>
2023-08-27 14:45:16 -0400

    nix: refactor sdk to be used in other projects

--- flake.nix
+++ flake.nix
@@ -19,55 +19,61 @@
         };
       in {
         devShells = {
-          default = pkgs.fcitx5-android-shell { };
-          noAS = pkgs.fcitx5-android-shell { enableAndroidStudio = false; };
+          default = pkgs.fcitx5-android.sdk.shell;
+          noAS =
+            pkgs.fcitx5-android.sdk.shell.override { androidStudio = null; };
         };
       }) // {
         overlays.default = final: prev: {
-          fcitx5-android-shell = { enableAndroidStudio ? true }:
-            with final;
-            with fcitx5-android-sdk;
-            mkShell {
-              buildInputs = [
-                androidComposition.androidsdk
-                extra-cmake-modules
-                gettext
-                python39
-                icu
-              ] ++ (if enableAndroidStudio then
-                [ androidStudioPackages.stable ]
-              else
-                [ ]);
-              ANDROID_SDK_ROOT =
-                "${androidComposition.androidsdk}/libexec/android-sdk";
-              NDK_VERSION = ndkVersion;
-              BUILD_TOOLS_VERSION = buildToolsVersion;
-              GRADLE_OPTS =
-                "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidComposition.androidsdk}/libexec/android-sdk/build-tools/${buildToolsVersion}/aapt2";
-              ECM_DIR = "${extra-cmake-modules}/share/ECM/cmake/";
-              JAVA_HOME = "${jdk17}";
-              shellHook = ''
-                export PATH="$ANDROID_SDK_ROOT/cmake/${cmakeVersion}/bin:$PATH"
-                echo sdk.dir=$ANDROID_SDK_ROOT > local.properties
-              '';
-            };
-          fcitx5-android-sdk = rec {
-            cmakeVersion = "3.22.1";
-            buildToolsVersion = "33.0.2";
-            platformToolsVersion = "33.0.3";
-            platformVersion = "33";
-            ndkVersion = "25.2.9519653";
-            abiVersions = [ "arm64-v8a" "armeabi-v7a" ];
-            androidComposition = prev.androidenv.composeAndroidPackages {
-              inherit platformToolsVersion ndkVersion;
-              buildToolsVersions = [ buildToolsVersion ];
-              platformVersions = [ platformVersion ];
-              inherit abiVersions;
-              cmakeVersions = [ cmakeVersion ];
+          fcitx5-android = {
+            sdk = final.lib.makeExtensible (self: {
+
+              # Update versions here
+              # This should match to build-logic/convention/src/main/kotlin/Versions.kt
+              cmakeVersion = "3.22.1";
+              buildToolsVersion = "33.0.2";
+              platformToolsVersion = "33.0.3";
+              platformVersion = "33";
+              ndkVersion = "25.2.9519653";
+
+              abiVersions = [ "arm64-v8a" "armeabi-v7a" ];
               includeNDK = true;
-              includeEmulator = false;
-              includeSources = true;
-            };
+              androidComposition = final.androidenv.composeAndroidPackages {
+                inherit (self)
+                  platformToolsVersion ndkVersion abiVersions includeNDK;
+                buildToolsVersions = [ self.buildToolsVersion ];
+                platformVersions = [ self.platformVersion ];
+                cmakeVersions = [ self.cmakeVersion ];
+                includeEmulator = false;
+                includeSources = true;
+              };
+
+              shell = final.lib.makeOverridable ({ androidStudio }:
+                with final;
+                with self;
+                mkShell {
+                  buildInputs = [
+                    androidComposition.androidsdk
+                    extra-cmake-modules
+                    gettext
+                    python39
+                    icu
+                    androidStudio
+                  ];
+                  ANDROID_SDK_ROOT =
+                    "${androidComposition.androidsdk}/libexec/android-sdk";
+                  NDK_VERSION = ndkVersion;
+                  BUILD_TOOLS_VERSION = buildToolsVersion;
+                  GRADLE_OPTS =
+                    "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidComposition.androidsdk}/libexec/android-sdk/build-tools/${buildToolsVersion}/aapt2";
+                  ECM_DIR = "${extra-cmake-modules}/share/ECM/cmake/";
+                  JAVA_HOME = "${jdk17}";
+                  shellHook = ''
+                    export PATH="$ANDROID_SDK_ROOT/cmake/${cmakeVersion}/bin:$PATH"
+                    echo sdk.dir=$ANDROID_SDK_ROOT > local.properties
+                  '';
+                }) { androidStudio = final.androidStudioPackages.stable; };
+            });
           };
         };
       };
