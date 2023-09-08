2ebe589e54b39ab6e74003f3046a7950bd7ee87c
Qijia Liu <liumeo@pku.edu.cn>
2023-09-08 00:35:22 -0400

    set default ECM_DIR to homebrew for macOS build

--- lib/fcitx5/src/main/cpp/cmake/FindECM.cmake
+++ lib/fcitx5/src/main/cpp/cmake/FindECM.cmake
@@ -1,5 +1,7 @@
 if(DEFINED ENV{ECM_DIR})
     set(ECM_DIR $ENV{ECM_DIR})
+elseif(CMAKE_HOST_APPLE)
+    set(ECM_DIR /opt/homebrew/share/ECM/cmake)
 else()
     set(ECM_DIR /usr/share/ECM/cmake)
 endif()
