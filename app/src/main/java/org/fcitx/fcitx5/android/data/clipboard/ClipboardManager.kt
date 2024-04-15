9ff20d6262a30d84ebb03f224504a8be363b6d34
Rocka <i@rocka.me>
2024-04-15 22:13:05 +0800

    Wipe the clipboard database instead of crashing when downgrade

--- app/src/main/java/org/fcitx/fcitx5/android/data/clipboard/ClipboardManager.kt
+++ app/src/main/java/org/fcitx/fcitx5/android/data/clipboard/ClipboardManager.kt
@@ -84,6 +84,8 @@ object ClipboardManager : ClipboardManager.OnPrimaryClipChangedListener,
     fun init(context: Context) {
         clbDb = Room
             .databaseBuilder(context, ClipboardDatabase::class.java, "clbdb")
+            // allow wipe the database instead of crashing when downgrade
+            .fallbackToDestructiveMigrationOnDowngrade()
             .build()
         clbDao = clbDb.clipboardDao()
         enabledListener.onChange(enabledPref.key, enabledPref.getValue())
