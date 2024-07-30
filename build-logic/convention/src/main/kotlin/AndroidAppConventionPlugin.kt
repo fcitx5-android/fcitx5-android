14cef4c75596aeec3050c99e351b08aca40b4e3d
Rocka <i@rocka.me>
2024-07-31 01:29:51 +0800

    Exclude redundant files from apk

--- build-logic/convention/src/main/kotlin/AndroidAppConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidAppConventionPlugin.kt
@@ -2,13 +2,20 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later
  * SPDX-FileCopyrightText: Copyright 2021-2023 Fcitx5 for Android Contributors
  */
+import com.android.build.api.dsl.ApplicationExtension
 import com.android.build.api.variant.ApplicationAndroidComponentsExtension
 import com.android.build.gradle.internal.dsl.BaseAppModuleExtension
+import com.android.build.gradle.tasks.PackageApplication
 import com.mikepenz.aboutlibraries.plugin.AboutLibrariesExtension
 import org.gradle.api.Project
+import org.gradle.api.file.RegularFile
+import org.gradle.api.internal.provider.Providers
 import org.gradle.configurationcache.extensions.capitalized
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.io.File
+import java.lang.reflect.Field
 
 /**
  * The prototype of an Android Application
@@ -52,6 +59,68 @@ class AndroidAppConventionPlugin : AndroidBaseConventionPlugin() {
             }
             compileOptions {
                 isCoreLibraryDesugaringEnabled = true
+            }
+        }
+
+        target.extensions.configure<ApplicationExtension> {
+            dependenciesInfo {
+                includeInApk = false
+                includeInBundle = false
+            }
+            packaging {
+                resources {
+                    excludes += setOf(
+                        "/META-INF/*.version",
+                        "/META-INF/*.kotlin_module",  // cannot be excluded actually
+                        "/kotlin/**",
+                        "/kotlin-tooling-metadata.json"
+                    )
+                }
+            }
+        }
+
+        // remove META-INF/com/android/build/gradle/app-metadata.properties
+        target.tasks.withType<PackageApplication> {
+            var javaClass: Class<*>? = appMetadata.javaClass
+            var valueField: Field? = null
+            while (javaClass != null) {
+                valueField = javaClass.declaredFields.find { it.name == "value" }
+                if (valueField != null) break
+                else javaClass = javaClass.superclass
+            }
+            valueField?.isAccessible = true
+            var appMetadataPath: String? = null
+            target.afterEvaluate {
+                // writeReleaseAppMetadata was skipped... but why?
+                if (appMetadata.isPresent) {
+                    appMetadata.asFile.get().apply {
+                        appMetadataPath = path
+                        parentFile.mkdirs()
+                        // make sure appMetadata file exists before the task
+                        writeText("")
+                    }
+                }
+            }
+            doFirst {
+                appMetadataPath?.let { File(it).delete() }
+                valueField?.set(appMetadata, Providers.notDefined<RegularFile>())
+                allInputFilesWithNameOnlyPathSensitivity.removeAll { true }
+            }
+        }
+
+        // try to remove <pkg_name>-<version_name>.kotlin_module, but it does not work ¯\_(ツ)_/¯
+        target.tasks.withType<KotlinCompile> {
+            doLast f@{
+                val ktClass = outputs.files.files.filter { it.path.contains("kotlin-classes") }
+                if (ktClass.isEmpty()) return@f
+                val metaInf = ktClass.first().resolve("META-INF")
+                if (!metaInf.exists() || !metaInf.isDirectory) return@f
+                metaInf.listFiles()?.forEach {
+                    if (it.name.endsWith(".kotlin_module")) {
+                        println("deleting ${it.path}")
+                        it.delete()
+                    }
+                }
             }
         }
 
