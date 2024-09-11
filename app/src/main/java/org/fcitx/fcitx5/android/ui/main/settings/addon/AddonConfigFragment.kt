96506d1ec9e405682d84919981eef27647a644d9
Rocka <i@rocka.me>
2024-09-11 21:26:40 +0800

    Fix crash when opening table addon config while it's not loaded

--- app/src/main/java/org/fcitx/fcitx5/android/ui/main/settings/addon/AddonConfigFragment.kt
+++ app/src/main/java/org/fcitx/fcitx5/android/ui/main/settings/addon/AddonConfigFragment.kt
@@ -16,14 +16,15 @@ class AddonConfigFragment : FcitxPreferenceFragment() {
         val addon = requireStringArg(ARG_UNIQUE_NAME)
         val raw = fcitx.getAddonConfig(addon)
         if (addon == "table") {
-            val desc = raw["desc"]["TableGlobalConfig"]
-            val androidTable = RawConfig(
-                "AndroidTable", subItems = arrayOf(
-                    RawConfig("Type", "External"),
-                    RawConfig("Description", getString(R.string.manage_table_im))
+            // append android specific "Manage Table Input Methods" to config of table addon
+            raw.findByName("desc")?.findByName("TableGlobalConfig")?.let {
+                it.subItems = (it.subItems ?: emptyArray()) + RawConfig(
+                    "AndroidTable", subItems = arrayOf(
+                        RawConfig("Type", "External"),
+                        RawConfig("Description", getString(R.string.manage_table_im))
+                    )
                 )
-            )
-            desc.subItems = (desc.subItems ?: arrayOf()) + androidTable
+            }
         }
         return raw
     }
