d535a320342f130d423ad3178ec35cf9edf2995a
Potato Hatsue <1793913507@qq.com>
2024-11-06 23:59:49 -0500

    nix: update sdk versions (#610)

--- flake.nix
+++ flake.nix
@@ -31,9 +31,9 @@
               # Update versions here
               # This should match to build-logic/convention/src/main/kotlin/Versions.kt
               cmakeVersion = "3.22.1";
-              buildToolsVersion = "34.0.0";
-              platformToolsVersion = "34.0.5";
-              platformVersion = "34";
+              buildToolsVersion = "35.0.0";
+              platformToolsVersion = "35.0.2";
+              platformVersion = "35";
               ndkVersion = "25.2.9519653";
 
               includeNDK = true;
