78c03d126124a9ec19cc5071eb94086737b0be55
Rocka <i@rocka.me>
2024-12-01 16:58:32 +0800

    Bump 0.1.0
    - Switch to Triple-T metadata structure, for plugin icons on F-Droid
    https://f-droid.org/docs/All_About_Descriptions_Graphics_and_Screenshots/#triple-t-structure
    - Fix japanese translation of app_name
    - Add icon for thai plugin

--- build-logic/convention/src/main/kotlin/Versions.kt
+++ build-logic/convention/src/main/kotlin/Versions.kt
@@ -17,8 +17,8 @@ object Versions {
     const val defaultBuildTools = "35.0.0"
 
     // NOTE: increase this value to bump version code
-    const val baseVersionCode = 7
-    const val baseVersionName = "0.0.9"
+    const val baseVersionCode = 8
+    const val baseVersionName = "0.1.0"
 
     val supportedABIs = setOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
     const val fallbackABI = "arm64-v8a"
