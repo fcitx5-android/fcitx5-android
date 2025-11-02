33353884fc9bb345fcdb799fdc50cf96ce450a1e
Rocka <i@rocka.me>
2025-11-02 14:33:04 +0800

    Make a common CMakeBuildInstallTask

--- build-logic/convention/src/main/kotlin/NativeBuildTasks.kt
+++ build-logic/convention/src/main/kotlin/NativeBuildTasks.kt
@@ -7,11 +7,18 @@ import com.android.build.gradle.internal.cxx.logging.PassThroughRecordingLogging
 import com.android.build.gradle.internal.cxx.model.CxxAbiModel
 import com.android.build.gradle.tasks.ExternalNativeBuildJsonTask
 import com.android.build.gradle.tasks.ExternalNativeBuildTask
+import org.gradle.api.Action
+import org.gradle.api.DefaultTask
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.TaskAction
 import org.gradle.kotlin.dsl.property
 import org.gradle.kotlin.dsl.withType
+import org.gradle.process.ExecSpec
+import java.io.File
 
 fun ExternalNativeBuildJsonTask.abiModel(): CxxAbiModel {
     val abi = ExternalNativeBuildJsonTask::class.java.declaredFields.find { it.name == "abi" }!!
@@ -38,6 +45,55 @@ fun Project.getCxxAbiModelProperty(): Property<CxxAbiModel> {
         }
     }
     return abiModel
+}
+
+abstract class CMakeBuildInstallTask : DefaultTask() {
+    @get:Input
+    @get:Optional
+    abstract val cxxAbiModel: Property<CxxAbiModel>
+
+    @get:Input
+    @get:Optional
+    abstract val buildTarget: Property<String>
+
+    @get:Input
+    @get:Optional
+    abstract val installComponent: Property<String>
+
+    @get:Input
+    abstract val destDir: Property<File>
+
+    private fun exec(action: Action<ExecSpec>) {
+        project.providers.exec(action).result.get()
+    }
+
+    @TaskAction
+    fun execute() {
+        val cxxAbiModel = this.cxxAbiModel.get()
+        val buildTarget = this.buildTarget.getOrElse("")
+        val installComponent = this.installComponent.getOrElse("")
+        val destDir = this.destDir.get()
+        val cmake = cxxAbiModel.variant.module.cmake!!.cmakeExe!!
+        if (buildTarget.isNotEmpty()) {
+            exec {
+                commandLine(
+                    cmake,
+                    "--build", cxxAbiModel.cxxBuildFolder,
+                    "--target", buildTarget
+                )
+            }
+        }
+        if (installComponent.isNotEmpty()) {
+            exec {
+                environment("DESTDIR", destDir.absolutePath)
+                commandLine(
+                    cmake,
+                    "--install", cxxAbiModel.cxxBuildFolder,
+                    "--component", installComponent
+                )
+            }
+        }
+    }
 }
 
 /**
