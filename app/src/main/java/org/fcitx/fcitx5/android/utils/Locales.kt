c3edd3fe63fd1cdc6d6a41632453e2c5d8591601
Rocka <i@rocka.me>
2026-09-13 00:15:57 +0800

    Fallback zh_*_#Hans/Hant to zh_CN/TW

--- app/src/main/java/org/fcitx/fcitx5/android/utils/Locales.kt
+++ app/src/main/java/org/fcitx/fcitx5/android/utils/Locales.kt
@@ -1,6 +1,6 @@
 /*
  * SPDX-License-Identifier: LGPL-2.1-or-later
- * SPDX-FileCopyrightText: Copyright 2021-2023 Fcitx5 for Android Contributors
+ * SPDX-FileCopyrightText: Copyright 2021-2026 Fcitx5 for Android Contributors
  */
 package org.fcitx.fcitx5.android.utils
 
@@ -19,24 +19,35 @@ object Locales {
         private set
 
     fun onLocaleChange(configuration: Configuration) {
-        fcitxLocale = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            configuration.locales.let {
-                buildString {
-                    for (i in 0 until it.size()) {
-                        if (i != 0) append(":")
-                        append(it[i].run { "${language}_${country}:$language" })
-                        // since there is not an `en.mo` file, `en` must be the only locale
-                        // in order to use default english translation
-                        if (i == 0 && it[i].language == "en") break
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            val locales = LinkedHashSet<String>()
+            val localeList = configuration.locales
+            for (i in 0..<localeList.size()) {
+                val it = localeList[i]
+                locales.add("${it.language}_${it.country}")
+                // fcitx5 only has zh_CN for simplified Chinese and zh_TW for traditional
+                if (it.language == "zh") {
+                    if (it.script == "Hans" && it.country != "CN") {
+                        locales.add("zh_CN")
+                    } else if (it.script == "Hant" && it.country != "TW") {
+                        locales.add("zh_TW")
                     }
                 }
+                locales.add("${it.language}")
+                // since there is not an `en.mo` file, `en` must be the only locale
+                // in order to use default English translation
+                if (i == 0 && it.language == "en") break
             }
+            languageWithCountry = locales.firstOrNull() ?: ""
+            language = languageWithCountry.substringBefore(':')
+            fcitxLocale = locales.joinToString(":")
         } else {
             @Suppress("DEPRECATION")
-            configuration.locale.run { "${language}_${country}:$language" }
+            val it = configuration.locale
+            languageWithCountry = "${it.language}_${it.country}"
+            language = it.language
+            fcitxLocale = "$languageWithCountry:$language"
         }
-        languageWithCountry = fcitxLocale.split(':')[0]
-        language = languageWithCountry.split('_')[0]
     }
 
 }
